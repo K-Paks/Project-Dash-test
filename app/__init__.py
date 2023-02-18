01b07e29c9e0e8eb82ecb643f3ed8a508e2e2c56
Rahul <saihanumarahul66@gmail.com>
2023-02-19 00:50:12 +0530

    Bump to 0.2.6
    Beautified allocator script
    Added Zoom In class for the Images if needed
    Have Updated Some Packages

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 # Above version is to note only the changes in particular to the app configuration
 
--- app/allocator.js
+++ app/allocator.js
@@ -2,36 +2,23 @@ const renderer = new DashRenderer();
 
 (function(){
     const url = document.URL;
+    const modules = {
+        swiper: ["https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"],
+        eCharts: ["https://cdn.jsdelivr.net/npm/echarts@5.4.1/dist/echarts.min.js"],
+        userView: ["/MLA/assets/view_dashboard.js", "/MLA/assets/view_dashboard_plots.js"]}
 
-    const extensions = [
-        "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js",
-        "https://cdn.jsdelivr.net/npm/echarts@5.4.1/dist/echarts.min.js",
-        "/MLA/assets/view_dashboard.js",
-        "/MLA/assets/view_dashboard_plots.js"
-    ]
-
-    const allowed_extensions = [
-        "https://unpkg.com/dash.nprogress@latest/dist/dash.nprogress.js"
-    ];
+    // Maintain the scripts based on the order
+    const allowed_extensions = ["https://unpkg.com/dash.nprogress@latest/dist/dash.nprogress.js"];
     const MLA_Prefixed = (page) => new RegExp(`^https?:\/\/.*\/MLA\/${page}`);
 
-    if(
-        MLA_Prefixed("view").test(url)
-    ){
-        allowed_extensions.push(extensions[0]);
-        allowed_extensions.push(extensions[1]);
-        allowed_extensions.push(extensions[2]);
-        allowed_extensions.push(extensions[3]);
-    }
+    if(MLA_Prefixed("view").test(url))
+        allowed_extensions.push(...modules.swiper, ...modules.eCharts, ...modules.userView);
 
     const head = document.getElementsByTagName('head')[0];
-
-
     allowed_extensions.forEach(function(scriptPath){
         const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = scriptPath;
+        script.type = 'text/javascript'; script.src = scriptPath;
         head.appendChild(script);
     });
-
 })()
+
